b59c96943c8a3f1321afebcb344a5b16c8738e5d
BlueNyang <xpsj20@gmail.com>
2025-09-21 22:07:53 +0900

    feat: user login

--- 5.back-end/Backend/WebGame2/src/main/java/kr/bluenyang/webgame/controller/user/UserLoginServlet.java
+++ 5.back-end/Backend/WebGame2/src/main/java/kr/bluenyang/webgame/controller/user/UserLoginServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kr.bluenyang.webgame.service.user.UserService;
 
 import java.io.IOException;
 import java.io.Serial;
@@ -14,6 +15,14 @@ import java.io.Serial;
 public class UserLoginServlet extends HttpServlet {
     @Serial
     private static final long serialVersionUID = 1L;
+    private UserService userService;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        
+        this.userService = (UserService) getServletContext().getAttribute("userService");
+    }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
---
+++ 5.back-end/Backend/WebGame2/src/main/java/kr/bluenyang/webgame/domain/user/User.java
@@ -0,0 +1,4 @@
+package kr.bluenyang.webgame.domain.user;
+
+public record User(String userId, String password, String username, String userEmail) {
+}
