20b76a63e2062e31d042fc9a9e9aa554da49a685
BlueNyang <xpsj20@gmail.com>
2025-10-01 17:17:49 +0900

    refactor: encryption -> security

--- 6.spring/SpringBoot/SpringBootEx/src/main/java/kr/bluenyang/practice/springbootex/config/SecurityConfig.java
+++ 6.spring/SpringBoot/SpringBootEx/src/main/java/kr/bluenyang/practice/springbootex/config/SecurityConfig.java
@@ -2,13 +2,28 @@ package kr.bluenyang.practice.springbootex.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // 스프링 기본 인증 작업 끄기
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable) // csrf 토큰 없이도 로그인 가능
+                .formLogin(FormLoginConfigurer::disable) // 스프링시큐리티 제공 기본 로그인 사용종료
+                .headers(headerConfig ->
+                        headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)); // iframe페이지는 허가하지 않음(X-Frame-Options 응답헤더 : DENY로 설정)
+        return http.build();
     }
 }
