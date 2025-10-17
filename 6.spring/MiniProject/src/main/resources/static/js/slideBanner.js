c1aa761ded85f8d1f087669a2d3a6779ffe25734
BlueNyang <xpsj20@gmail.com>
2025-10-17 14:45:42 +0900

    feat: swiper

--- 6.spring/MiniProject/src/main/resources/static/js/slideBanner.js
+++ 6.spring/MiniProject/src/main/resources/static/js/slideBanner.js
@@ -1,14 +1,66 @@
-(() => {
-  document.addEventListener("DOMContentLoaded", async () => {
-    const resp = await fetch("/get/slides");
-    const slides = await resp.json();
+let swiper;
 
+(() => {
+  const addBannerItems = (slideImgs) => {
     const slideWrapper = document.querySelector(".swiper-wrapper");
-    slides.forEach((slide) => {
+
+    slideImgs.forEach((slideImg) => {
       const slideDiv = document.createElement("div");
       slideDiv.classList.add("swiper-slide");
-      slideDiv.innerHTML = `<img src="${slide.image}" alt="${slide.alt}" />`;
+      slideDiv.dataset.title = slideImg.imageTitle;
+      slideDiv.dataset.alt = slideImg.imageAlt;
+
+      const a = document.createElement("a");
+      a.href = slideImg.linkUrl || "#";
+      a.innerHTML = `<img src="${slideImg.imageUrl}" alt="[${slideImg.imageTitle}] ${slideImg.imageAlt}" />`;
+
+      slideDiv.appendChild(a);
       slideWrapper.appendChild(slideDiv);
+    });
+  };
+
+  document.addEventListener("DOMContentLoaded", async () => {
+    const resp = await fetch("/get/slides");
+    const slideImgs = await resp.json();
+
+    addBannerItems(slideImgs);
+    if (slideImgs.length <= 3) {
+      addBannerItems(slideImgs);
+    }
+
+    swiper = new Swiper(".swiper", {
+      loop: true,
+
+      autoplay: {
+        delay: 3000,
+      },
+
+      slidesPerView: "auto",
+      centeredSlides: true,
+      spaceBetween: 0,
+
+      on: {
+        slideChange: function () {
+          const realIndex = this.realIndex;
+          const currentSlide = this.slides.find(
+            (slide) =>
+              parseInt(slide.getAttribute("data-swiper-slide-index")) ===
+              realIndex
+          );
+
+          if (currentSlide) {
+            const title = currentSlide.dataset.title;
+            const alt = currentSlide.dataset.alt;
+
+            document.querySelector(".alt-title").textContent = title;
+            document.querySelector(".alt-desc").textContent = alt;
+          }
+        },
+
+        init: function () {
+          this.emit("slideChange");
+        },
+      },
     });
   });
 })();
