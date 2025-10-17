90f88760308b0df34e3742a29a49561f5a61e33e
BlueNyang <xpsj20@gmail.com>
2025-10-17 11:32:51 +0900

    feat: spring boot controller

--- 6.spring/SpringBoot/SpringBootReact/src/main/java/kr/bluenyang/practice/springbootreact/product/repository/ProductRepository.java
+++ 6.spring/SpringBoot/SpringBootReact/src/main/java/kr/bluenyang/practice/springbootreact/product/repository/ProductRepository.java
@@ -3,20 +3,7 @@ package kr.bluenyang.practice.springbootreact.product.repository;
 import kr.bluenyang.practice.springbootreact.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 //@Repository
 public interface ProductRepository extends JpaRepository<Product, String>, JpaSpecificationExecutor<Product> {
-    @Query("""
-            SELECT p
-              FROM Product p
-              WHERE (:type = 'prdName' AND p.prdName LIKE concat('%', :keyword, '%'))
-                 OR (:type = 'prdCompany'AND p.prdCompany LIKE concat('%',:keyword,'%'))
-              ORDER BY p.prdNo
-            """)
-    List<Product> searchProduct(@Param("type") String type,
-                                @Param("keyword") String keyword);
 }
