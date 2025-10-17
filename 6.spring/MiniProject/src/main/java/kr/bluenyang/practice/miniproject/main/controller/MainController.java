bc2281a0809a14535551d7791711f4ba50c6393d
BlueNyang <xpsj20@gmail.com>
2025-10-17 14:45:32 +0900

    fix: service dependency

--- 6.spring/MiniProject/src/main/java/kr/bluenyang/practice/miniproject/main/controller/MainController.java
+++ 6.spring/MiniProject/src/main/java/kr/bluenyang/practice/miniproject/main/controller/MainController.java
@@ -3,21 +3,16 @@ package kr.bluenyang.practice.miniproject.main.controller;
 import kr.bluenyang.practice.miniproject.main.model.BannerImageDTO;
 import kr.bluenyang.practice.miniproject.main.service.MainService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
-    MainService service;
+    private final MainService service;
 
     @GetMapping("/")
     public String index() {
