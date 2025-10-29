72c667f31c558a248d4982589e14a0548e43ba25
BlueNyang <xpsj20@gmail.com>
2025-10-29 11:42:48 +0900

    feat: config-server

---
+++ 7.msa/SpringBootMSA/config-server/src/test/java/kr/bluenyang/practice/msa/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.bluenyang.practice.msa.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
