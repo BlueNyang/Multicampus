8bb7360c8695a60d138829067a12dd592c6281ee
BlueNyang <xpsj20@gmail.com>
2025-10-29 13:49:40 +0900

    chore: eureka server

--- 7.msa/SpringBootMSA/gateway/src/main/java/kr/bluenyang/practice/msa/gateway/GatewayApplication.java
+++ 7.msa/SpringBootMSA/gateway/src/main/java/kr/bluenyang/practice/msa/gateway/GatewayApplication.java
@@ -1,11 +1,23 @@
 package kr.bluenyang.practice.msa.gateway;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class GatewayApplication {
+
+    @Value("${post.api.uri}")
+    private static String postUri;
+
+    @Value("${comment.api.uri}")
+    private static String commentUri;
+
     public static void main(String[] args) {
+        log.info("postUri: {}, commentUri: {}", postUri, commentUri);
+
         SpringApplication.run(GatewayApplication.class, args);
     }
 }
