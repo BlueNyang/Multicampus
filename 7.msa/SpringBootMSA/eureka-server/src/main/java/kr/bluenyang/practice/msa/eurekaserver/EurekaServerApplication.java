aef8a3efbfbb65f06761b4bdb4a611779f30aed7
BlueNyang <xpsj20@gmail.com>
2025-10-29 13:49:40 +0900

    chore: eureka server

---
+++ 7.msa/SpringBootMSA/eureka-server/src/main/java/kr/bluenyang/practice/msa/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package kr.bluenyang.practice.msa.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
---
+++ 7.msa/SpringBootMSA/eureka-server/src/test/java/kr/bluenyang/practice/msa/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.bluenyang.practice.msa.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
